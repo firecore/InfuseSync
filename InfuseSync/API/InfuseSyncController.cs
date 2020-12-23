f31aa6499fa71f67654ebe3e0d631b5944faf7d6
firecore <dev@firecore.com>
2020-12-23 15:07:07 +0400

    Add ASP.NET Core MVC controller for Jellyfin 10.7.0 (#16337)

---
+++ InfuseSync/API/InfuseSyncController.cs
@@ -0,0 +1,228 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using InfuseSync.Models;
+using MediaBrowser.Common.Extensions;
+using MediaBrowser.Controller.Dto;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Dto;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Querying;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InfuseSync.API
+{
+    /// <summary>
+    /// ASP.NET Core MVC controller for Jellyfin plugin.
+    /// Wraps <see cref="InfuseSyncService">.
+    /// </summary>
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class InfuseSyncController : ControllerBase
+    {
+        private readonly InfuseSyncService _service;
+
+        public InfuseSyncController(
+            ILogger<InfuseSyncController> logger,
+            IUserManager userManager,
+            IUserDataManager userDataManager,
+            ILibraryManager libraryManager,
+            IDtoService dtoService)
+        {
+            _service = new InfuseSyncService
+            (
+                logger,
+                userManager,
+                userDataManager,
+                libraryManager,
+                dtoService
+            );
+        }
+
+        /// <summary>
+        /// Creates new synchronization checkpoint and removes previous device checkpoints.
+        /// </summary>
+        /// <param name="deviceId">Unique device identifier.</param>
+        /// <param name="userId">User identifier.</param>
+        /// <returns>A <see cref="CheckpointId"/>.</returns>
+        [HttpPost("InfuseSync/Checkpoint")]
+        public ActionResult<CheckpointId> CreateCheckpoint(
+            [FromQuery] string deviceId,
+            [FromQuery] string userId)
+        {
+            var request = new CreateCheckpoint
+            {
+                DeviceID = deviceId,
+                UserID = userId
+            };
+            return _service.Post(request);
+        }
+
+        /// <summary>
+        /// Starts synchronization session for a checkpoint and returns items statistics.
+        /// </summary>
+        /// <param name="checkpointID">Checkpoint identifier.</param>
+        /// <returns>A <see cref="SyncStats"/>.</returns>
+        [HttpPost("InfuseSync/Checkpoint/{checkpointID}/StartSync")]
+        public ActionResult<SyncStats> StartCheckpointSync([FromRoute] Guid checkpointID)
+        {
+            var request = new StartCheckpointSync
+            {
+                CheckpointID = checkpointID
+            };
+            return _service.Post(request);
+        }
+
+        /// <summary>
+        /// Get updated items for {checkpointId}.
+        /// </summary>
+        /// <param name="checkpointId">The checkpoint ID.</param>
+        /// <param name="includeItemTypes">List of item types to include in the result.</param>
+        /// <param name="fields">Additional fields of information to return in the output. This allows multiple, comma delimeted values.</param>
+        /// <param name="startIndex">Offset for items to fetch.</param>
+        /// <param name="limit">Maximum number of items to fetch.</param>
+        /// <returns>The <see cref="QueryResult"/> with the list of <see cref="BaseItemDto"/> for updated items.</returns>
+        [HttpGet("InfuseSync/Checkpoint/{checkpointId}/UpdatedItems")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<QueryResult<BaseItemDto>> GetUpdatedItemsQuery(
+            [FromRoute] Guid checkpointId,
+            [FromQuery] string? includeItemTypes,
+            [FromQuery] string fields,
+            [FromQuery] int? startIndex,
+            [FromQuery] int? limit)
+        {
+            var request = new GetUpdatedItemsQuery
+            {
+                CheckpointID = checkpointId,
+                IncludeItemTypes = includeItemTypes,
+                Fields = fields,
+                StartIndex = startIndex,
+                Limit = limit
+            };
+
+            try
+            {
+                return _service.Get(request);
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (ResourceNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get removed item IDs for {checkpointID}.
+        /// </summary>
+        /// <param name="checkpointId">The checkpoint ID.</param>
+        /// <param name="includeItemTypes">List of item types to include in the result.</param>
+        /// <param name="startIndex">Offset for items to fetch.</param>
+        /// <param name="limit">Maximum number of items to fetch.</param>
+        /// <returns>The <see cref="QueryResult"/> with the list of <see cref="RemovedItem"/>.</returns>
+        [HttpGet("/InfuseSync/Checkpoint/{checkpointID}/RemovedItems")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<QueryResult<RemovedItem>> GetRemovedItemsQuery(
+            [FromRoute] Guid checkpointId,
+            [FromQuery] string? includeItemTypes,
+            [FromQuery] int? startIndex,
+            [FromQuery] int? limit)
+        {
+            var request = new GetRemovedItemsQuery
+            {
+                CheckpointID = checkpointId,
+                IncludeItemTypes = includeItemTypes,
+                StartIndex = startIndex,
+                Limit = limit
+            };
+
+            try
+            {
+                return _service.Get(request);
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (ResourceNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get updated user data for {checkpointID}.
+        /// </summary>
+        /// <param name="checkpointId">The checkpoint ID.</param>
+        /// <param name="includeItemTypes">List of item types to include in the result.</param>
+        /// <param name="startIndex">Offset for items to fetch.</param>
+        /// <param name="limit">Maximum number of items to fetch.</param>
+        /// <returns>The <see cref="QueryResult"/> with the list of <see cref="RemovedItem"/>.</returns>
+        [HttpGet("/InfuseSync/Checkpoint/{checkpointID}/UserData")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<QueryResult<UserItemDataDto>> GetUserDataQuery(
+            [FromRoute] Guid checkpointId,
+            [FromQuery] string? includeItemTypes,
+            [FromQuery] int? startIndex,
+            [FromQuery] int? limit)
+        {
+            var request = new GetUserDataQuery
+            {
+                CheckpointID = checkpointId,
+                IncludeItemTypes = includeItemTypes,
+                StartIndex = startIndex,
+                Limit = limit
+            };
+
+            try
+            {
+                return _service.Get(request);
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (ResourceNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of user libraries and folders.
+        /// </summary>
+        /// <param name="userID">User identifier.</param>
+        /// <returns>List of <see cref="VirtualFolderInfo"/>.</returns>
+        [HttpGet("InfuseSync/UserFolders/{userID}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<VirtualFolderInfo>> GetUserFolders([FromRoute] string userId)
+        {
+            var request = new GetUserFolders
+            {
+                UserID = userId
+            };
+
+            try
+            {
+                return _service.Get(request);
+            }
+            catch (ResourceNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+    }
+}
--- InfuseSync/API/InfuseSyncService.cs
+++ InfuseSync/API/InfuseSyncService.cs
@@ -1,37 +1,76 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
+using InfuseSync.Models;
 using MediaBrowser.Common.Extensions;
-using MediaBrowser.Controller.Dto;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Controller.Net;
+using MediaBrowser.Controller.Dto;
 using MediaBrowser.Model.Dto;
-using MediaBrowser.Model.Services;
 using MediaBrowser.Model.Querying;
-using InfuseSync.Models;
 
 #if EMBY
+using MediaBrowser.Model.Services;
+using MediaBrowser.Controller.Net;
 using InfuseSync.Logging;
 using ILogger = MediaBrowser.Model.Logging.ILogger;
 #else
 using Jellyfin.Data.Entities;
-using System.Globalization;
+using MediaBrowser.Model.Entities;
 using Microsoft.Extensions.Logging;
-using ILogger = Microsoft.Extensions.Logging.ILogger<InfuseSync.API.InfuseSyncItems>;
+using System.Globalization;
 #endif
 
 namespace InfuseSync.API
 {
+#if EMBY
+    [Route("/InfuseSync/Checkpoint", "POST", Summary = "Create new synchronization checkpoint and remove previous device checkpoints")]
+    [Authenticated]
+#endif
+    public class CreateCheckpoint
+#if EMBY
+     : IReturn<CheckpointId>
+#endif
+    {
+#if EMBY
+        [ApiMember(Name = "DeviceID", Description = "Unique device identifier", IsRequired = true, DataType = "string", ParameterType = "query", Verb = "POST")]
+        [ApiMember(Name = "UserID", Description = "User identifier", IsRequired = true, DataType = "string", ParameterType = "query", Verb = "POST")]
+#endif
+        public string DeviceID { get; set; }
+        public string UserID { get; set; }
+    }
+
+#if EMBY
+    [Route("/InfuseSync/Checkpoint/{CheckpointID}/StartSync", "POST", Summary = "Start synchronization session for a checkpoint and return items statistics")]
+    [Authenticated]
+#endif
+    public class StartCheckpointSync
+#if EMBY
+     : IReturn<SyncStats>
+#endif
+    {
+#if EMBY
+        [ApiMember(Name = "CheckpointID", Description = "Checkpoint identifier", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "POST")]
+#endif
+        public Guid CheckpointID { get; set; }
+    }
+
+#if EMBY
     [Route("/InfuseSync/Checkpoint/{CheckpointID}/UpdatedItems", "GET", Summary = "Get updated items for {CheckpointID}")]
     [Authenticated]
-    public class GetUpdatedItemsQuery : IReturn<QueryResult<ItemRec>>
+#endif
+    public class GetUpdatedItemsQuery
+#if EMBY
+     : IReturn<QueryResult<ItemRec>>
+#endif
     {
+#if EMBY
         [ApiMember(Name = "CheckpointID", Description = "Checkpoint identifier", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "GET")]
         [ApiMember(Name = "IncludeItemTypes", Description = "Optional list of item types to include in the result", IsRequired = false, DataType = "string", ParameterType = "query", Verb = "GET")]
         [ApiMember(Name = "Fields", Description = "Optional. Specify additional fields of information to return in the output. This allows multiple, comma delimeted values.", IsRequired = false, DataType = "string", ParameterType = "query", Verb = "GET", AllowMultiple = true)]
         [ApiMember(Name = "StartIndex", Description = "Offset for items to fetch", IsRequired = false, DataType = "int", ParameterType = "query", Verb = "GET")]
         [ApiMember(Name = "Limit", Description = "Maximum number of items to fetch", IsRequired = false, DataType = "int", ParameterType = "query", Verb = "GET")]
+#endif
         public Guid CheckpointID { get; set; }
         public string IncludeItemTypes { get; set; }
         public string Fields { get; set; }
@@ -56,50 +95,126 @@ namespace InfuseSync.API
         }
     }
 
+#if EMBY
     [Route("/InfuseSync/Checkpoint/{CheckpointID}/RemovedItems", "GET", Summary = "Get removed item IDs for {CheckpointID}")]
     [Authenticated]
-    public class GetRemovedItemsQuery : IReturn<QueryResult<RemovedItem>>
+#endif
+    public class GetRemovedItemsQuery
+#if EMBY
+     : IReturn<QueryResult<RemovedItem>>
+#endif
     {
+#if EMBY
         [ApiMember(Name = "CheckpointID", Description = "Checkpoint identifier", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "GET")]
         [ApiMember(Name = "IncludeItemTypes", Description = "Optional list of item types to include in the result", IsRequired = false, DataType = "string", ParameterType = "query", Verb = "GET")]
         [ApiMember(Name = "StartIndex", Description = "Offset for items to fetch", IsRequired = false, DataType = "int", ParameterType = "query", Verb = "GET")]
         [ApiMember(Name = "Limit", Description = "Maximum number of items to fetch", IsRequired = false, DataType = "int", ParameterType = "query", Verb = "GET")]
+#endif
         public Guid CheckpointID { get; set; }
         public string IncludeItemTypes { get; set; }
         public int? StartIndex { get; set; }
         public int? Limit { get; set; }
     }
 
+#if EMBY
     [Route("/InfuseSync/Checkpoint/{CheckpointID}/UserData", "GET", Summary = "Get updated user data for {CheckpointID}")]
     [Authenticated]
-    public class GetUserDataQuery : IReturn<QueryResult<UserItemDataDto>>
+#endif
+    public class GetUserDataQuery
+#if EMBY
+     : IReturn<QueryResult<UserItemDataDto>>
+#endif
     {
+#if EMBY
         [ApiMember(Name = "CheckpointID", Description = "Checkpoint identifier", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "GET")]
         [ApiMember(Name = "IncludeItemTypes", Description = "Optional list of item types to include in the result", IsRequired = false, DataType = "string", ParameterType = "query", Verb = "GET")]
         [ApiMember(Name = "StartIndex", Description = "Offset for items to fetch", IsRequired = false, DataType = "int", ParameterType = "query", Verb = "GET")]
         [ApiMember(Name = "Limit", Description = "Maximum number of items to fetch", IsRequired = false, DataType = "int", ParameterType = "query", Verb = "GET")]
+#endif
         public Guid CheckpointID { get; set; }
         public string IncludeItemTypes { get; set; }
         public int? StartIndex { get; set; }
         public int? Limit { get; set; }
     }
 
-    public class InfuseSyncItems : IService
+    public class GetUserFolders
+    {
+        public string UserID { get; set; }
+    }
+
+    public class InfuseSyncService
+#if EMBY
+     : IService
+#endif
     {
         private readonly ILogger _logger;
-
         private readonly IUserManager _userManager;
         private readonly IUserDataManager _userDataManager;
         private readonly ILibraryManager _libraryManager;
         private readonly IDtoService _dtoService;
 
-        public InfuseSyncItems(ILogger logger, IUserManager userManager, IUserDataManager userDataManager, ILibraryManager libraryManager, IDtoService dtoService)
+        public InfuseSyncService(
+            ILogger logger,
+            IUserManager userManager,
+            IUserDataManager userDataManager,
+            ILibraryManager libraryManager,
+            IDtoService dtoService)
         {
             _logger = logger;
             _userManager = userManager;
             _userDataManager = userDataManager;
             _libraryManager = libraryManager;
             _dtoService = dtoService;
+        }
+
+        public CheckpointId Post(CreateCheckpoint request)
+        {
+            _logger.LogDebug($"InfuseSync: Create checkpoint request for DeviceID '{request.DeviceID}' UserID '{request.UserID}'");
+
+            var newCheckpoint = Plugin.Instance.Db.CreateCheckpoint(request.DeviceID, request.UserID);
+
+            return new CheckpointId { Id = newCheckpoint.Guid };
+        }
+
+        public SyncStats Post(StartCheckpointSync request)
+        {
+            _logger.LogDebug($"InfuseSync: Sync request for CheckpointID '{request.CheckpointID}'");
+
+            var checkpoint = Plugin.Instance.Db.GetCheckpoint(request.CheckpointID);
+            if (checkpoint == null)
+            {
+                throw new ResourceNotFoundException($"Checkpoint with ID '{request.CheckpointID}' not found.");
+            }
+
+            var db = Plugin.Instance.Db;
+
+            var syncTimestamp = DateTime.UtcNow.ToFileTime();
+            db.UpdateCheckpoint(request.CheckpointID, syncTimestamp);
+
+            var folderTypes = new string [] {"Folder"};
+            var boxSetTypes = new string [] {"BoxSet"};
+            var playlistTypes = new string [] {"Playlist"};
+            var seriesTypes = new string [] {"Series"};
+            var seasonTypes = new string [] {"Season"};
+            var collectionFolderTypes = new string [] {"CollectionFolder"};
+            var videoTypes = new string [] {"Video", "MusicVideo", "Movie", "Episode"};
+
+            return new SyncStats {
+                UpdatedFolders = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Updated, folderTypes),
+                RemovedFolders = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Removed, folderTypes),
+                UpdatedBoxSets = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Updated, boxSetTypes),
+                RemovedBoxSets = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Removed, boxSetTypes),
+                UpdatedPlaylists = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Updated, playlistTypes),
+                RemovedPlaylists = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Removed, playlistTypes),
+                UpdatedTvShows = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Updated, seriesTypes),
+                RemovedTvShows = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Removed, seriesTypes),
+                UpdatedSeasons = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Updated, seasonTypes),
+                RemovedSeasons = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Removed, seasonTypes),
+                UpdatedVideos = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Updated, videoTypes),
+                RemovedVideos = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Removed, videoTypes),
+                UpdatedCollectionFolders = db.ItemsCount(checkpoint.Timestamp, syncTimestamp, ItemStatus.Updated, collectionFolderTypes),
+                UpdatedUserData = db.UserInfoCount(checkpoint.Timestamp, syncTimestamp, checkpoint.UserId, videoTypes)
+            };
         }
 
         public QueryResult<BaseItemDto> Get(GetUpdatedItemsQuery request)
@@ -135,6 +250,10 @@ namespace InfuseSync.API
             );
 
             var user = _userManager.GetUserById(Guid.Parse(checkpoint.UserId));
+            if (user == null)
+            {
+                throw new ResourceNotFoundException($"User not found for checkpoint with ID '{request.CheckpointID}'.");
+            }
 
             var items = GetUserItems(user, itemsUpdated);
 
@@ -252,6 +371,10 @@ namespace InfuseSync.API
             );
 
             var user = _userManager.GetUserById(Guid.Parse(checkpoint.UserId));
+            if (user == null)
+            {
+                throw new ResourceNotFoundException($"User not found for checkpoint with ID '{request.CheckpointID}'.");
+            }
 
             var userData = updatedUserData
 #if EMBY
@@ -279,5 +402,26 @@ namespace InfuseSync.API
                 TotalRecordCount = totalCount
             };
         }
+
+#if JELLYFIN
+        public List<VirtualFolderInfo> Get(GetUserFolders request)
+        {
+            _logger.LogDebug($"InfuseSync: User folders requested for UserID '{request.UserID}'");
+
+            var user = _userManager.GetUserById(Guid.Parse(request.UserID));
+            if (user == null)
+            {
+                throw new ResourceNotFoundException($"User with ID '{request.UserID}' not found.");
+            }
+
+            return _libraryManager.GetVirtualFolders()
+                .Where(f =>
+                {
+                    var item = _libraryManager.GetItemById(f.ItemId);
+                    return item != null && item.IsVisibleStandalone(user);
+                })
+                .ToList();
+        }
+#endif
     }
 }
