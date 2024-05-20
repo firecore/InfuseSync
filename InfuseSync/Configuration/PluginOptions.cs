8437d93b5bfbf2b49b50df5a08582f52295a7058
firecore <dev@firecore.com>
2024-05-20 14:13:27 +0400

    Use BasePluginSimpleUI for Emby options (#21593)

---
+++ InfuseSync/Configuration/PluginOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace InfuseSync.Configuration
+{
+    using System.ComponentModel;
+    using Emby.Web.GenericEdit;
+
+    public class PluginOptions : EditableOptionsBase
+    {
+        public override string EditorTitle => "Infuse Sync";
+
+        [DisplayName("Cache Expiration")]
+        [Description("Maximum days to keep cached data")]
+        public int CacheExpirationDays { get; set; }
+
+        public PluginOptions()
+        {
+            CacheExpirationDays = 30;
+        }
+    }
+}
